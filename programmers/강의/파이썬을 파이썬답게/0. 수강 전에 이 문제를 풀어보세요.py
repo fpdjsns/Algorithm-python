ec38226a6a0ac2f08714c0ffb9372e419a04095d
withham <withham1@naver.com>
2021-09-25 12:24:32 +0900

    Delete 0. 수강 전에 이 문제를 풀어보세요.py

--- programmers/강의/파이썬을 파이썬답게/0. 수강 전에 이 문제를 풀어보세요.py
+++
@@ -1,8 +0,0 @@
-# problem : https://programmers.co.kr/learn/courses/4008/lessons/13254
-# time complexity : O(N). N = |mylist|
-
-def solution(mylist):
-    answer = []
-    for list in mylist:
-        answer.append(len(list))
-    return answer
