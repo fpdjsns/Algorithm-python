2f1574822ceb1c3bf320f461c6f5700a9ede16c3
withham <withham1@naver.com>
2022-01-08 12:48:11 +0900

    Create input-output.py

---
+++ tip/input-output.py
@@ -0,0 +1,5 @@
+# https://codingcompetitions.withgoogle.com/codejam/faq
+t = int(input())
+for i in range(1, t + 1):
+  n, m = [int(s) for s in input().split(" ")]
+  print("Case #{}: {} {}".format(i, n + m, n * m))
